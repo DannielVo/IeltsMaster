b652f9d057d408b16bef842a9b26973cdcd8e51f
DannielVo <phattinhoc2017@gmail.com>
2025-07-03 16:08:34 +0700

    Feat: finish responsive header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,65 @@
 import React from "react";
 import "./header.css";
 import { assets } from "../../assets/asset";
+import useMobileNav from "../../hooks/useMobileNav";
 
 const Header = () => {
-  return (
-    <header>
-      <div className="header-container">
-        <div className="nav-left">
-          <a href="home.html" className="logo">
-            <img src={assets.logo} alt="Logo" />
-          </a>
-        </div>
+  const { isMobileNavOpen, toggleMobileNav, mobileNavRef, headerBarsRef } =
+    useMobileNav();
 
-        <div className="nav-right">
-          <a href="#">Flashcards</a>
-          <a href="#">Academic</a>
-          <a href="#">General</a>
-          <a href="#">Sign in</a>
+  return (
+    <>
+      <header>
+        <div className="header-container">
+          <div className="nav-left">
+            <a href="home.html" className="logo">
+              <img src={assets.logo} alt="Logo" />
+            </a>
+          </div>
+
+          <div className="nav-right">
+            <a href="#">Flashcards</a>
+            <a href="#">Academic</a>
+            <a href="#">General</a>
+            <a href="#">Sign in</a>
+            <button type="button" ref={headerBarsRef} onClick={toggleMobileNav}>
+              <i class="bx  bx-menu"></i>
+            </button>
+          </div>
         </div>
+      </header>
+
+      {/* Mobile */}
+      <div ref={mobileNavRef} className="mobile-header">
+        <nav>
+          <ul>
+            <li>
+              {" "}
+              <a href="#">Flashcards</a>
+            </li>
+
+            <li>
+              {" "}
+              <a href="#">Academic</a>
+            </li>
+
+            <li>
+              {" "}
+              <a href="#">General</a>
+            </li>
+
+            <li>
+              {" "}
+              <a href="#">Sign in</a>
+            </li>
+          </ul>
+        </nav>
+
+        <button type="button" onClick={toggleMobileNav}>
+          <i class="bx  bx-x"></i>
+        </button>
       </div>
-    </header>
+    </>
   );
 };
 
