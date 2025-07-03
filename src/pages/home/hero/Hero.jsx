9dbcbbcd696ea2bee0fbbc39f307633ddd9ff1e0
DannielVo <phattinhoc2017@gmail.com>
2025-07-03 20:42:36 +0700

    Feat: Finish responsive features-review in hero section

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./hero.css";
 import Introduction from "./introduction/Introduction";
+import FeaturesReview from "./features-review/FeaturesReview";
 
 const Hero = () => {
   return (
     <section className="hero left-right">
       <Introduction></Introduction>
+      <FeaturesReview></FeaturesReview>
     </section>
   );
 };
---
+++ src/pages/home/hero/features-review/FeaturesReview.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import "./featuresReview.css";
+
+const FeaturesReview = () => {
+  return (
+    <div className="features-review">
+      <h2>What Awaits You at IELTS Master?</h2>
+
+      <div className="feature-review-content">
+        <div className="feature-left">
+          <div className="feature-item">
+            <i className="bxr bx-trophy-star feature-icon"></i>
+            <div className="feature-content">
+              <h3>Practice Smarter with Flashcards</h3>
+              <p>
+                Use our built-in flashcards — or create your own — to memorize
+                vocabulary, grammar rules, or even speaking answers. Your
+                personal learning toolkit, always within reach.
+              </p>
+            </div>
+          </div>
+
+          <div className="feature-item">
+            <i className="bxr bx-price-tag feature-icon"></i>
+            <div className="feature-content">
+              <h3>Practice Smarter with Flashcards</h3>
+              <p>
+                Use our built-in flashcards — or create your own — to memorize
+                vocabulary, grammar rules, or even speaking answers. Your
+                personal learning toolkit, always within reach.
+              </p>
+            </div>
+          </div>
+
+          <div className="feature-item">
+            <i className="bxr bx-book-open feature-icon"></i>
+            <div className="feature-content">
+              <h3>Practice Smarter with Flashcards</h3>
+              <p>
+                Use our built-in flashcards — or create your own — to memorize
+                vocabulary, grammar rules, or even speaking answers. Your
+                personal learning toolkit, always within reach.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="feature right">
+          <div className="feature-item">
+            <i className="bxr bx-refresh-cw-alt feature-icon"></i>
+            <div className="feature-content">
+              <h3>Practice Smarter with Flashcards</h3>
+              <p>
+                Use our built-in flashcards — or create your own — to memorize
+                vocabulary, grammar rules, or even speaking answers. Your
+                personal learning toolkit, always within reach.
+              </p>
+            </div>
+          </div>
+
+          <div className="feature-item">
+            <i className="bxr bx-fire-alt feature-icon"></i>
+            <div className="feature-content">
+              <h3>Practice Smarter with Flashcards</h3>
+              <p>
+                Use our built-in flashcards — or create your own — to memorize
+                vocabulary, grammar rules, or even speaking answers. Your
+                personal learning toolkit, always within reach.
+              </p>
+            </div>
+          </div>
+
+          <div className="feature-item">
+            <i className="bxr bx-community feature-icon"></i>
+            <div className="feature-content">
+              <h3>Practice Smarter with Flashcards</h3>
+              <p>
+                Use our built-in flashcards — or create your own — to memorize
+                vocabulary, grammar rules, or even speaking answers. Your
+                personal learning toolkit, always within reach.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturesReview;
